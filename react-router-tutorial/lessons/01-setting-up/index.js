38998cf50cde40ee9397d417b7fab5e6d5f9138e
Zero Tung <zero_tung@163.com>
2017-01-11 13:35:23 +0800

    学完第6节 可以通过添加 activeStyle 或 activeClassName 来修改当前渲染的页面的链接样式 由于需要知道哪个链接正在被渲染的需求主要只在 navigator 上，所以将其抽象出来 可以使用 /repos/:userName/:repoName 匹配类似 /repos/reactjs/react-router 路径 路径中的"reactjs"和"react-router"将在渲染页面中通过this.props.params.userName(repoName)调用

--- react-router-tutorial/lessons/01-setting-up/index.js
+++ react-router-tutorial/lessons/01-setting-up/index.js
@@ -11,11 +11,14 @@ import {
 
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render((
     <Router history={hashHistory}>
         <Route path="/" component={App}>
             <Route path="/repos" component={Repos} />
+            {}
+            <Route path="/repos/:userName/:repoName" component={Repo} />
             <Route path="/about" component={About} />
         </Route>
     </Router>
