cd728f9a85cacdfc3f2bbdd77becffffd4072fce
Zero Tung <zero_tung@163.com>
2017-01-11 12:49:35 +0800

    学完react-router第四节 通过引入Link from react-router，使用Link进行跳转。 将子页面放在主页面下，主页面渲染navbar。 子页面通过主页面在navbar下面渲染，可以实现navbar的共用。

--- react-router-tutorial/lessons/01-setting-up/modules/App.js
+++ react-router-tutorial/lessons/01-setting-up/modules/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {
+    Link
+} from 'react-router'
 
 export default React.createClass({
-  render() {
-    return <div>Hello, React Router!</div>
-  }
+    render() {
+        return (
+            <div>
+                <h1>React Router Tutorial</h1>
+                <ul role="nav">
+                    <li><Link to="/">index</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/repos">Repos</Link></li>
+                </ul>
+
+                {this.props.children}
+            </div>
+        )
+    }
 })
